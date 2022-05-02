10e2f85886cf4515067f740a439c795cdb8bc49a
FlutLab.io <>
2022-05-02 19:17:41 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(KFCApp());
 } //Funcion principal
 
-class MyApp extends StatelessWidget {
+class KFCApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
